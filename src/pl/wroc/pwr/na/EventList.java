5b09a94435e48977c40ed2f04883d4cf3b4a249a
Zygmunt Horodyski <zygota.v2@gmail.com>
2012-11-30 17:31:56 +0100

    changed few things :)

--- src/pl/wroc/pwr/na/EventList.java
+++ src/pl/wroc/pwr/na/EventList.java
@@ -1,18 +1,25 @@
 package pl.wroc.pwr.na;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.TextView;
 
 public class EventList extends Activity implements OnClickListener {
 	
-	public static String LIST_TITLE = "";
+	public static final String LIST_TITLE = "pl.wroc.pwr.na.list_title";
+	
+	private TextView title;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.eventlist);
+		
+		title = (TextView) findViewById(R.id.eventlist_title);
+		title.setText(getIntent().getStringExtra(LIST_TITLE));
 	}
 
 	@Override
--- src/pl/wroc/pwr/na/NAPWr.java
+++ src/pl/wroc/pwr/na/NAPWr.java
@@ -13,31 +13,78 @@ import android.widget.Button;
 
 public class NAPWr extends Activity implements OnClickListener {
 
+	private int ACTIVE_OPTION;
+
+	Button top10;
+	Button today;
+	Button tommorow;
+	Button callendar;
+	Button favourites;
+	Button login;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
-		if(Preferences.getFulscreen(this)){
+
+		ACTIVE_OPTION = 2;
+
+		if (Preferences.getFulscreen(this)) {
 			requestWindowFeature(Window.FEATURE_NO_TITLE);
-	        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, 
-	                                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+			getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+					WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		}
-		
+
 		setContentView(R.layout.main);
 
 		// odszukujemy przyciski i podpinamy pod nie listenery
-		Button top10 = (Button) findViewById(R.id.main_button_top10);
+		top10 = (Button) findViewById(R.id.main_button_top10);
 		top10.setOnClickListener(this);
-		Button today = (Button) findViewById(R.id.main_button_today);
+		today = (Button) findViewById(R.id.main_button_today);
 		today.setOnClickListener(this);
-		Button tommorow = (Button) findViewById(R.id.main_button_tommorow);
+		tommorow = (Button) findViewById(R.id.main_button_tommorow);
 		tommorow.setOnClickListener(this);
-		Button callendar = (Button) findViewById(R.id.main_button_callendar);
+		callendar = (Button) findViewById(R.id.main_button_callendar);
 		callendar.setOnClickListener(this);
-		Button favourites = (Button) findViewById(R.id.main_button_favourites);
+		favourites = (Button) findViewById(R.id.main_button_favourites);
 		favourites.setOnClickListener(this);
-		Button login = (Button) findViewById(R.id.main_button_login);
+		login = (Button) findViewById(R.id.main_button_login);
 		login.setOnClickListener(this);
+		
+		makeAtive(ACTIVE_OPTION);
+	}
+
+	public void makeAtive(int i) {
+		Button b = today;
+		switch (i) {
+		case 1:
+			b = top10;
+			break;
+		case 2:
+			b = today;
+			break;
+		case 3:
+			b = tommorow;
+			break;
+		case 4:
+			b = callendar;
+			break;
+		case 5:
+			b = favourites;
+			break;
+		case 6:
+			b = login;
+			break;
+		}
+		b.setBackgroundResource(R.drawable.custom_button_selected);
+	}
+	
+	public void makeBasicBackground(){
+		top10.setBackgroundResource(R.drawable.custom_button);
+		today.setBackgroundResource(R.drawable.custom_button);
+		tommorow.setBackgroundResource(R.drawable.custom_button);
+		callendar.setBackgroundResource(R.drawable.custom_button);
+		favourites.setBackgroundResource(R.drawable.custom_button);
+		login.setBackgroundResource(R.drawable.custom_button);
 	}
 
 	@Override
@@ -70,8 +117,8 @@ public class NAPWr extends Activity implements OnClickListener {
 			break;
 		}
 	}
-	
-	public void startList(String name){
+
+	public void startList(String name) {
 		Intent i = new Intent(this, EventList.class);
 		i.putExtra(EventList.LIST_TITLE, name);
 		startActivity(i);
