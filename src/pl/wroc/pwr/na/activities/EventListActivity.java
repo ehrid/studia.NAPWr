b42da18e9bd43044d58bf4ff478258abaca2a69e
Konrad Masalski <konradmas@gmail.com>
2012-12-20 01:33:53 +0100

    pierwsza przymiarka do sciągania listy wydarzeń z serwera

--- src/pl/wroc/pwr/na/activities/EventListActivity.java
+++ src/pl/wroc/pwr/na/activities/EventListActivity.java
@@ -1,8 +1,23 @@
 package pl.wroc.pwr.na.activities;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import pl.wroc.pwr.na.R;
 import pl.wroc.pwr.na.adapters.EventListAdapter;
@@ -11,6 +26,7 @@ import pl.wroc.pwr.na.objects.EventObject;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -75,19 +91,76 @@ public class EventListActivity extends Activity implements OnClickListener {
 	}
 
 	public void addEvents() {
+		String wydarzenieTytul = "sciema";
+		String wydarzenieTresc = "sciema";
+		try {
+			//new RequestTask().execute("http://na.pwr.wroc.pl/mobile/wydarzenia/jutro");
+			
+			JSONArray completeJSONArr = new JSONArray((String) new RequestTask().execute("http://na.pwr.wroc.pl/mobile/wydarzenia/jutro").get());
+			JSONObject object = completeJSONArr.getJSONObject(0);
+			wydarzenieTytul = object.getString("wydarzenieTytul");
+			wydarzenieTresc = object.getString("wydarzenieTresc");
+			
+			
+		} catch (JSONException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			// Log.d(TAG, "httpclient.execute failed");
+			e.printStackTrace();
+			
+		}
+
 		ArrayList<EventObject> eventList = new ArrayList<EventObject>();
 
-		for(int i=1; i < 10; i++)
-		{
-			eventList.add(new EventObject("Wydarznie nr: "+i, i,
-					"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam in pretium eros. Integer enim dolor, suscipit eget cursus a, fringilla sit amet justo. Aenean vel lorem ipsum, id tristique nulla. Proin eu felis enim, eu congue tortor... ",
-					"http://www.na.pwr.wroc.pl/Symfony/web/bundles/cona/img/plakaty/small-5733534440398.jpg",7*i, new Date()));
+		for (int i = 1; i < 10; i++) {
+			eventList
+					.add(new EventObject(
+							wydarzenieTytul,
+							i,
+							wydarzenieTresc,
+							"http://www.na.pwr.wroc.pl/Symfony/web/bundles/cona/img/plakaty/small-5733534440398.jpg",
+							7 * i, new Date()));
 		}
-		
 
 		adapter = new EventListAdapter(this, R.layout.item_event_list,
 				eventList);
 		eventListView.setAdapter(adapter);
+	}
+
+	class RequestTask extends AsyncTask<String, String, String> {
+
+		@Override
+		protected String doInBackground(String... uri) {
+			HttpClient httpclient = new DefaultHttpClient();
+			HttpResponse response;
+			String responseString = null;
+			try {
+				response = httpclient.execute(new HttpGet(uri[0]));
+				StatusLine statusLine = response.getStatusLine();
+				if (statusLine.getStatusCode() == HttpStatus.SC_OK) {
+					ByteArrayOutputStream out = new ByteArrayOutputStream();
+					response.getEntity().writeTo(out);
+					
+					responseString = out.toString();
+					out.close();
+				} else {
+					// Closes the connection.
+					response.getEntity().getContent().close();
+					throw new IOException(statusLine.getReasonPhrase()+ uri[0]);
+				}
+			} catch (ClientProtocolException e) {
+				// TODO Handle problems..
+			} catch (IOException e) {
+				
+			}
+			return responseString;
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			super.onPostExecute(result);
+			// Do anything with response..
+		}
 	}
 
 }
